5134443fb8ca762a68453dec2ab6b8544814d2e9
billchen198318 <chen.xin.nien@gmail.com>
2023-07-14 22:13:31 +0800

    update

--- frontend/src/components/FullName.svelte
+++ frontend/src/components/FullName.svelte
@@ -1,6 +1,13 @@
+<script>
+    import { Button } from 'sveltestrap';    
+    import { toast, SvelteToast } from '@zerodevx/svelte-toast'    
+
+    export let params = {};
+    
+</script>
+
 <p>You gave us both a first name and last name!</p>
 <p>First: {params.first}</p>
 <p>Last: {params.last}</p>
-<script>
-export let params = {}
-</script>
+<Button on:click={() => toast.push('你他媽真是個天才!')}>SHOW TOAST</Button>
+<SvelteToast/>
--- frontend/src/components/ShortName.svelte
+++ frontend/src/components/ShortName.svelte
@@ -1,5 +1,11 @@
+<script>
+    import { Button } from 'sveltestrap';    
+    import { toast, SvelteToast } from '@zerodevx/svelte-toast'   
+    
+    export let params = {}
+    
+</script>
 <p>You shy person, giving us a first name only!</p>
 <p>First: {params.first}</p>
-<script>
-export let params = {}
-</script>
+<Button on:click={() => toast.push('Hello world!')}>SHOW TOAST</Button>
+<SvelteToast/>
