13882e444b5d77e0d412fe9a9b1d5888096bc39b
Bill Chen <chen.xin.nien@gmail.com>
2023-07-20 14:03:37 +0800

    update

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -224,7 +224,7 @@
         {/each}
         
        	<li><a class="app-menu__item" href="#/about" ><Icon name="info-circle" />&nbsp;<span class="app-menu__label">About</span></a></li>
-       	<li><a class="app-menu__item" href="#" ><Icon name="door-open" />&nbsp;<span class="app-menu__label">Logout</span></a></li>
+       	<li><a class="app-menu__item" href="#" on:click={userLogout} ><Icon name="door-open" />&nbsp;<span class="app-menu__label">Logout</span></a></li>
        	        
         
       </ul>
