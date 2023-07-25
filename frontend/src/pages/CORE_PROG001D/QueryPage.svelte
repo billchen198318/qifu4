14624241fc23e18eec6df326df94bc0d4b9f0310
billchen198318 <chen.xin.nien@gmail.com>
2023-07-25 21:50:59 +0800

    update

--- frontend/src/pages/CORE_PROG001D/QueryPage.svelte
+++ frontend/src/pages/CORE_PROG001D/QueryPage.svelte
@@ -3,7 +3,11 @@ import { onMount } from 'svelte';
 import { getProgItem } from "../../components/BaseHelper.svelte";
 import { push } from 'svelte-spa-router';
 import Toolbar from "../../components/Toolbar.svelte";
-import { Form, FormGroup, FormText, Input, Label } from 'sveltestrap';
+import { 
+    Form, FormGroup, FormText, Input, Label,
+    Pagination, PaginationItem, PaginationLink,
+    Table 
+} from 'sveltestrap';
 
 let toolbarParam = {
     id          : 'CORE_PROG001D0001Q',
@@ -54,5 +58,70 @@ $ : {
             </FormGroup>      
         </Form>
     </div>
+
+    <div class="col-xs-12 col-md-12 col-lg-12">
+        <Table bordered hover>
+            <thead style="background-color:silver;">
+            <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </Table>
+      
+    </div>
+
+    <Pagination ariaLabel="Page navigation example">
+        <PaginationItem disabled>
+          <PaginationLink first href="#" />
+        </PaginationItem>
+        <PaginationItem disabled>
+          <PaginationLink previous href="#" />
+        </PaginationItem>
+        <PaginationItem active>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">2</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">4</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">5</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink next href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink last href="#" />
+        </PaginationItem>
+      </Pagination>
+
 
 </div>
