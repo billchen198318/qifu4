706a547cdebb5fad0028031a8bf82948fec68eab
billchen198318 <chen.xin.nien@gmail.com>
2023-08-10 22:13:25 +0800

    update

--- frontend/src/pages/CORE_PROG001D/IndexPage.svelte
+++ frontend/src/pages/CORE_PROG001D/IndexPage.svelte
@@ -5,16 +5,7 @@ import QueryPage from "./QueryPage.svelte";
 import CreatePage from "./CreatePage.svelte";
 import EditPage from "./EditPage.svelte";
 
-/*
-const prefix = '/prog001';
-const routes = new Map()
-        .set("/", QueryPage)
-        .set("/create", CreatePage)
-        .set("/edit/:oid", EditPage)
-        .set(/^\/(.*?)(\/(.*?))?/, QueryPage);
-*/
 const prefix = getProgFolderUrlPrefix('CORE_PROG001D');
-//alert('prefix=' + prefix);
 const routes = getNestedRoutesMap(QueryPage, CreatePage, EditPage);
 
 </script>
