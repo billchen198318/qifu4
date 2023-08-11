ec175dc79e912bc72c69e09c6922cf9b52d1b2f5
Bill Chen <chen.xin.nien@gmail.com>
2023-08-11 09:56:08 +0800

    update

--- frontend/src/pages/CORE_PROG001D/QueryPage.svelte
+++ frontend/src/pages/CORE_PROG001D/QueryPage.svelte
@@ -2,7 +2,7 @@
 import { onMount } from 'svelte';    
 import { push } from 'svelte-spa-router';
 import { 
-    Form, FormGroup, Input, Label, Button, Icon
+	Form, FormGroup, Input, Label, Button, Icon
 } from 'sveltestrap';
 import { toast, SvelteToast } from '@zerodevx/svelte-toast';
 import Swal from 'sweetalert2';
@@ -33,201 +33,204 @@ let toolbarParam = {
 let queryParam = { };
 let gridConfig = { };
 _queryParam.subscribe(value => {
-  queryParam = value;
+	queryParam = value;
 });
 _gridConfig.subscribe(value => {
-  gridConfig = value;
+	gridConfig = value;
 });
 /* -------------------------------------------------------- */
 
 if (Object.keys(queryParam).length == 0) {
-  queryParam = {
-    'sysId' : '',
-    'name'  : ''
-  }
+	queryParam = {
+		'sysId' : '',
+		'name'  : ''
+	}
 }
 if (Object.keys(gridConfig).length == 0) {
     gridConfig = getGridConfig(
-    'oid'
-    ,
-    [
-        {
-          'method'  : function(val) { 
-            var url = getProgItem('CORE_PROG001D0001E').url + '/' + val;
-            push( url );
-          },
-          'icon'    : 'pen',
-          'type'    : 'edit',
-          'memo'    : 'Edit current item.'
-        },
-        {
-          'method'  : function(val) { 
-            Swal.fire({
-              title: '刪除?',
-              icon: 'question',
-              iconHtml: '?',
-              confirmButtonText: 'Yes',
-              cancelButtonText: 'No',
-              showCancelButton: true,
-              showCloseButton: true
-            }).then((result) => {
-              if (result.isConfirmed) {
-                delItem(val);
-              }
-            });            
-          },
-          'icon'    : 'trash',
-          'type'    : 'delete',
-          'memo'    : 'Delete current item.'
-        },      
-    ]
-    ,
-    [
-      {
-        'label' : '#',
-        'field' : 'oid'
-      },
-      {
-        'label' : 'Id',
-        'field' : 'sysId'
-      },
-      {
-        'label' : 'Name',
-        'field' : 'name'
-      },
-      {
-        'label' : 'Host',
-        'field' : 'host'
-      },
-      {
-        'label' : 'Context Path',
-        'field' : 'contextPath'
-      },
-      {
-        'label' : 'Local',
-        'field' : 'isLocal'
-      }
-    ]    
-  );
-
+		'oid'
+		,
+		[
+			{
+				'method'  : function(val) { 
+					var url = getProgItem('CORE_PROG001D0001E').url + '/' + val;
+					push( url );
+				},
+				'icon'    : 'pen',
+				'type'    : 'edit',
+				'memo'    : 'Edit current item.'
+			},
+			{
+				'method'  : function(val) { 
+					Swal.fire({
+						title: '刪除?',
+						icon: 'question',
+						iconHtml: '?',
+						confirmButtonText: 'Yes',
+						cancelButtonText: 'No',
+						showCancelButton: true,
+						showCloseButton: true
+					}).then((result) => {
+						if (result.isConfirmed) {
+							delItem(val);
+						}
+					});            
+				},
+				'icon'    : 'trash',
+				'type'    : 'delete',
+				'memo'    : 'Delete current item.'
+			},      
+		]
+		,
+		[
+			{
+				'label' : '#',
+				'field' : 'oid'
+			}
+			,
+			{
+				'label' : 'Id',
+				'field' : 'sysId'
+			}
+			,
+			{
+				'label' : 'Name',
+				'field' : 'name'
+			}
+			,
+			{
+				'label' : 'Host',
+				'field' : 'host'
+			}
+			,
+			{
+				'label' : 'Context Path',
+				'field' : 'contextPath'
+			}
+			,
+			{
+				'label' : 'Local',
+				'field' : 'isLocal'
+			}
+		]    
+	);
 }
 
 let dsList = [];
 
 function changeQueryGridRow(row) {
-  setConfigRow(gridConfig, row);
-  gridConfig.page = 1;
-  gridConfig = gridConfig; // 讓 svelte child compomenet 知道 gridConfig被修改了
-  btnQuery();
+	setConfigRow(gridConfig, row);
+	gridConfig.page = 1;
+	gridConfig = gridConfig; // 讓 svelte child compomenet 知道 gridConfig被修改了
+	btnQuery();
 }
 
 function changePageSelect(page) {
-  setConfigPage(gridConfig, page);
-  gridConfig = gridConfig; // 讓 svelte child compomenet 知道 gridConfig被修改了
-  btnQuery();
+	setConfigPage(gridConfig, page);
+	gridConfig = gridConfig; // 讓 svelte child compomenet 知道 gridConfig被修改了
+	btnQuery();
 }
 
 function btnQuery() {
-  Swal.fire({title: "Loading...", html: "請等待", showConfirmButton: false, allowOutsideClick: false});
-  Swal.showLoading();
-
-  dsList = [];
-  dsList = dsList;
-  var axiosInstance = getAxiosInstance();
-  axiosInstance.post(import.meta.env.VITE_API_URL + '/prog001/findPage', {
-    "field": {
-      "sysId"     : queryParam.sysId,
-      "nameLike"  : queryParam.name
-    },
-    "pageOf": {
-      "select"  : gridConfig.page,
-      "showRow" : gridConfig.row
-    }
-  })
-  .then(response => {
-    Swal.hideLoading();
-    Swal.close();
-    if (null != response.data) {
-      if (import.meta.env.VITE_SUCCESS_FLAG != response.data.success) {
-        clearGridConfig();
-        toast.push(response.data.message, getToastWarningTheme());
-        return;
-      }
-      dsList = response.data.value;
-      setConfigTotal(gridConfig, response.data.pageOf.countSize);
-      gridConfig = gridConfig; // 讓 GridPagination 知道 gridConfig 被更動了
+	Swal.fire({title: "Loading...", html: "請等待", showConfirmButton: false, allowOutsideClick: false});
+	Swal.showLoading();
+	
+	dsList = [];
+	dsList = dsList;
+	var axiosInstance = getAxiosInstance();
+	axiosInstance.post(import.meta.env.VITE_API_URL + '/prog001/findPage', {
+		"field": {
+			"sysId"     : queryParam.sysId,
+			"nameLike"  : queryParam.name
+		}
+		,
+		"pageOf": {
+			"select"  : gridConfig.page,
+			"showRow" : gridConfig.row
+		}
+	})
+	.then(response => {
+		Swal.hideLoading();
+		Swal.close();
+		if (null != response.data) {
+			if (import.meta.env.VITE_SUCCESS_FLAG != response.data.success) {
+				clearGridConfig();
+				toast.push(response.data.message, getToastWarningTheme());
+				return;
+			}
+			dsList = response.data.value;
+			setConfigTotal(gridConfig, response.data.pageOf.countSize);
+			gridConfig = gridConfig; // 讓 GridPagination 知道 gridConfig 被更動了
       
-      /* -------------------- set page state -------------------- */
-      _gridConfig.update((value) => { return gridConfig;});
-      /* -------------------------------------------------------- */
+			/* -------------------- set page state -------------------- */
+			_gridConfig.update((value) => { return gridConfig;});
+			/* -------------------------------------------------------- */
       
-    } else {
-      toast.push('error, null', getToastErrorTheme());
-      clearGridConfig();
-    }
-  })
-  .catch(e => {
-    Swal.hideLoading();
-    Swal.close();    
-    clearGridConfig();
-    alert(e);
-  });
+		} else {
+			toast.push('error, null', getToastErrorTheme());
+			clearGridConfig();
+		}
+	})
+	.catch(e => {
+		Swal.hideLoading();
+		Swal.close();    
+		clearGridConfig();
+		alert(e);
+	});
 }
 
 function clearGridConfig() {
-  setConfigRow(gridConfig, import.meta.env.VITE_DEFAULT_ROW);
-  setConfigPage(gridConfig, 1);
-  setConfigTotal(gridConfig, 0);
-  gridConfig = gridConfig; // 讓 GridPagination 知道 gridConfig 被更動了
+	setConfigRow(gridConfig, import.meta.env.VITE_DEFAULT_ROW);
+	setConfigPage(gridConfig, 1);
+	setConfigTotal(gridConfig, 0);
+	gridConfig = gridConfig; // 讓 GridPagination 知道 gridConfig 被更動了
 }
 
 function btnClear() {
-  clearGridConfig();  
-  queryParam.name = '';
-  queryParam.sysId = '';
-  dsList = [];
+	clearGridConfig();  
+	queryParam.name = '';
+	queryParam.sysId = '';
+	dsList = [];
 }
 
 function delItem(oid) {
-  Swal.fire({title: "Loading...", html: "請等待", showConfirmButton: false, allowOutsideClick: false});
-  Swal.showLoading();  
-  var axiosInstance = getAxiosInstance();  
-  axiosInstance.post(import.meta.env.VITE_API_URL + '/prog001/delete', {"oid": oid})
-  .then(response => {
-    Swal.hideLoading();
-    Swal.close();
-    if (null != response.data) {
-      if (import.meta.env.VITE_SUCCESS_FLAG == response.data.success) {
-        toast.push(response.data.message, getToastDefaultTheme());
-      } else {        
-        toast.push(response.data.message, getToastWarningTheme());
-      }      
-      btnQuery();
-    } else {
-      toast.push('error, null', getToastErrorTheme());
-      clearGridConfig();
-    }
-  })
-  .catch(e => {
-    Swal.hideLoading();
-    Swal.close();    
-    btnQuery();
-    alert(e);
-  });  
+	Swal.fire({title: "Loading...", html: "請等待", showConfirmButton: false, allowOutsideClick: false});
+	Swal.showLoading();  
+	var axiosInstance = getAxiosInstance();  
+	axiosInstance.post(import.meta.env.VITE_API_URL + '/prog001/delete', {"oid": oid})
+	.then(response => {
+		Swal.hideLoading();
+		Swal.close();
+		if (null != response.data) {
+			if (import.meta.env.VITE_SUCCESS_FLAG == response.data.success) {
+				toast.push(response.data.message, getToastDefaultTheme());
+			} else {        
+				toast.push(response.data.message, getToastWarningTheme());
+			}      
+			btnQuery();
+		} else {
+			toast.push('error, null', getToastErrorTheme());
+			clearGridConfig();
+		}
+	})
+	.catch(e => {
+		Swal.hideLoading();
+		Swal.close();    
+		btnQuery();
+		alert(e);
+	});  
 }
 
 onMount(()=>{
-
-  btnQuery();
-
+	btnQuery();
 });
 
 </script>
 
 <div class="row">
-  <div class="col-xs-12 col-md-12 col-lg-12">
-    <Toolbar args={toolbarParam}></Toolbar>
-  </div>  
+	<div class="col-xs-12 col-md-12 col-lg-12">
+		<Toolbar args={toolbarParam}></Toolbar>
+	</div>  
 </div>
 <div class="row">
     <div class="col-xs-6 col-md-6 col-lg-6">
@@ -259,19 +262,19 @@ onMount(()=>{
 </div>
 <div class="row">
   <div class="col-xs-12 col-md-12 col-lg-12">
-      <Button color="primary" on:click={btnQuery}><Icon name="search"/>&nbsp;查詢</Button>
-      &nbsp;
-      <Button color="primary" on:click={btnClear}><Icon name="eraser"/>&nbsp;清除</Button>
+		<Button color="primary" on:click={btnQuery}><Icon name="search"/>&nbsp;查詢</Button>
+		&nbsp;
+		<Button color="primary" on:click={btnClear}><Icon name="eraser"/>&nbsp;清除</Button>
   </div>  
 </div>
 <div class="row">
-  <div class="col-xs-12 col-md-12 col-lg-12">&nbsp;</div>
+	<div class="col-xs-12 col-md-12 col-lg-12">&nbsp;</div>
 </div>
 <div class="row">
-  <div class="col-xs-12 col-md-12 col-lg-12">
-    <GridPagination changeGridConfigRowMethod={changeQueryGridRow} changePageSelectMethod={changePageSelect} gridConfig={gridConfig} />
-    <Grid config={gridConfig} bind:dataSource={dsList} />  
-  </div>
+	<div class="col-xs-12 col-md-12 col-lg-12">
+		<GridPagination changeGridConfigRowMethod={changeQueryGridRow} changePageSelectMethod={changePageSelect} gridConfig={gridConfig} />
+		<Grid config={gridConfig} bind:dataSource={dsList} />  
+	</div>
 </div>
 
 <SvelteToast/>
