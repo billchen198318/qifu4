194cceb476a5161d6840024c51572f81aa052ac3
Bill Chen <chen.xin.nien@gmail.com>
2023-08-11 16:33:36 +0800

    update

--- frontend/src/pages/About.svelte
+++ frontend/src/pages/About.svelte
@@ -19,8 +19,8 @@ import {
                 <CardText>
                     <h5>qífū&nbsp;-&nbsp;4</h5>
                     <br>
-                    qífū4&nbsp;是一個前端使用&nbsp;Svelte&nbsp;，&nbsp;後端使用&nbsp;springboot&nbsp;的一個泛例網站程式。<br>
-                    主要為了當作內部OA系統開發使用平台的基底，而設計的程式。<br>
+                    qífū4&nbsp;是一個前端使用&nbsp;Svelte&nbsp;，&nbsp;後端使用&nbsp;springboot&nbsp;的一個範例網站程式。<br>
+                    主要為了當作內部OA系統，開發使用的平台的基底，而設計的程式。<br>
                     <br>
                     Contact:&nbsp;chen.xin.nien@gmail.com<br>
                     GitHub:&nbsp;https://github.com/billchen198318/qifu4/<br>
