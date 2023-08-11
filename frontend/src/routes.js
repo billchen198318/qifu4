0278377e71733ade148c428ab07fe3ad91de4653
Bill Chen <chen.xin.nien@gmail.com>
2023-08-11 15:39:32 +0800

    update

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,16 +1,21 @@
-import { wrap } from 'svelte-spa-router/wrap'
-import PROG001 from "./pages/CORE_PROG001D/IndexPage.svelte";
+import {
+	wrap
+} from 'svelte-spa-router/wrap'
 
-export const routes = {  
-  "/prog001"    : PROG001,
-  "/prog001/*"  : PROG001,
-  "/about"      : wrap({
-    asyncComponent: () => import('./pages/About.svelte')    
-  }),
-  "/"           : wrap({
-    asyncComponent: () => import('./pages/About.svelte')    
-  }),
-  "*"           : wrap({
-    asyncComponent: () => import('./pages/NotFound.svelte')
-  })
+export const routes = {
+	"/prog001": wrap({
+		asyncComponent: () => import('./pages/CORE_PROG001D/IndexPage.svelte')
+	}),
+	"/prog001/*": wrap({
+		asyncComponent: () => import('./pages/CORE_PROG001D/IndexPage.svelte')
+	}),
+	"/about": wrap({
+		asyncComponent: () => import('./pages/About.svelte')
+	}),
+	"/": wrap({
+		asyncComponent: () => import('./pages/About.svelte')
+	}),
+	"*": wrap({
+		asyncComponent: () => import('./pages/NotFound.svelte')
+	})
 };
