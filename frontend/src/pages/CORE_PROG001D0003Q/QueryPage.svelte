eea4bcb26b32c4e0c90979b6fba9028032b57550
Bill Chen <chen.xin.nien@gmail.com>
2023-08-16 11:27:21 +0800

    update

--- frontend/src/pages/CORE_PROG001D0003Q/QueryPage.svelte
+++ frontend/src/pages/CORE_PROG001D0003Q/QueryPage.svelte
@@ -2,7 +2,7 @@
 import { onMount, onDestroy } from 'svelte';    
 import { push } from 'svelte-spa-router';
 import { 
-	FormGroup, Input, Label, Button, Icon
+	FormGroup, Input, Label, Button, Icon, Table
 } from 'sveltestrap';
 import { toast, SvelteToast } from '@zerodevx/svelte-toast';
 import Swal from 'sweetalert2';
@@ -58,16 +58,16 @@ function loadProgramFolder() {
     });
 }
 
-function programFolderChange() {
+function programFolderChange(e) {
     itemAllList = [];
     itemEnableList = [];    
-    if (import.meta.env.VITE_PLEASE_SELECT_ID == queryParam.folderOid) {
+    if (import.meta.env.VITE_PLEASE_SELECT_ID == e.target.value) {
         return;
     }
     Swal.fire({title: "Loading...", html: "請等待", showConfirmButton: false, allowOutsideClick: false});
     Swal.showLoading(); 
     let axiosInstance = getAxiosInstance();
-    axiosInstance.post(import.meta.env.VITE_API_URL + PageConstants.eventNamespace + '/loadProgramEnableAndAllList/' + queryParam.folderOid)
+    axiosInstance.post(import.meta.env.VITE_API_URL + PageConstants.eventNamespace + '/loadProgramEnableAndAllList/' + e.target.value)
     .then(response => {
         Swal.hideLoading();
         Swal.close();
@@ -88,6 +88,31 @@ function programFolderChange() {
         Swal.close();        
         alert(e);        
     });
+}
+
+function programItemEnableChange(e, itemOid) {
+    var checked = e.target.checked;
+    
+}
+
+function checkItemDisable(itemOid) {
+    var f = false;
+    for (var n in itemAllList) {
+        if (itemAllList[n].oid == itemOid && itemAllList[n].itemType == 'FOLDER') {
+            f = true;
+        }
+    }
+    return f;    
+}
+
+function checkItemChecked(itemOid) {
+    var f = false;
+    for (var n in itemEnableList) {
+        if (itemEnableList[n].oid == itemOid) {
+            f = true;
+        }
+    }
+    return f;
 }
 
 onMount(()=>{
@@ -117,6 +142,33 @@ onDestroy(()=>{
         </FormGroup>         
     </div>
 </div>
+<div class="row">
+    <div class="col-xs-12 col-md-12 col-lg-12">
 
+        <Table bordered hover>
+            <thead>
+                <tr>
+                    <th style="background-color: #575757; color: whitesmoke;">#</th>
+                    <th style="background-color: #575757; color: whitesmoke;">頁面程式名稱</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each itemAllList as item}
+                <tr>
+                    <td>
+                        <FormGroup>
+                            <div class="form-check">
+                            <Input type="checkbox" on:change={programItemEnableChange(event, item.oid)} checked={checkItemChecked(item.oid)} disabled={checkItemDisable(item.oid)} />
+                            </div>
+                        </FormGroup>
+                    </td>
+                    <td>{item.progId}&nbsp;-&nbsp;{item.name}</td>
+                </tr>
+                {/each}
+            </tbody>
+        </Table>
+
+    </div>
+</div>
 
 <SvelteToast/>
