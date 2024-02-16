941a57a2361a5870c75518b0a0b3b492071a5d25
billchen198318 <chen.xin.nien@gmail.com>
2024-02-16 21:38:36 +0800

    update

---
+++ frontend-v-nx/error.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+    error: {
+        type: Object,
+        required: true
+    }
+});
+</script>
+
+<template>
+<div class="row">
+    <div class="col-xs-12 col-md-12 col-lg-12">
+        <div class="card text-center">
+            <div class="card-header">
+                <i class="bi bi-cone-striped"></i>&nbsp;Error!!
+            </div>
+            <div class="card-body">
+                <p class="card-text">
+                    <h2>{{ error.statusCode }}</h2><br>
+                    <pre>{{ error.message }}</pre><br>
+                    <NuxtLink to="/">回首頁</NuxtLink>
+                </p>
+            </div>
+            <div class="card-footer text-muted">
+                Status code:&nbsp;{{ error.statusCode }}
+            </div>
+        </div>
+	</div>  
+</div>
+</template>
