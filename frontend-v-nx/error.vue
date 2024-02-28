49ae35073ecfb83e7d94e5467dd73b1363af1274
billchen198318 <chen.xin.nien@gmail.com>
2024-02-28 19:42:34 +0800

    update

--- frontend-v-nx/error.vue
+++ frontend-v-nx/error.vue
@@ -10,8 +10,8 @@ const props = defineProps({
 <template>
     <div class="callout callout-danger">
         <h4><i class="bi bi-cone-striped"></i>&nbsp;Error!!</h4>
-        <h2>{{ error.statusCode }}</h2><br>
-        <pre>{{ error.message }}</pre><br>
+        <h2>{{ error.statusCode }}</h2>
+        <pre>{{ error.message }}</pre>
         <NuxtLink to="/">回首頁</NuxtLink>
     </div>
 </template>
