5d769b79eccb880413261d5eb6935065c60d426c
billchen198318 <chen.xin.nien@gmail.com>
2024-04-23 22:10:42 +0800

    update

--- backend/app/src/main/java/org/qifu/controller/BlankController.java
+++ backend/app/src/main/java/org/qifu/controller/BlankController.java
@@ -1,18 +1,35 @@
 package org.qifu.controller;
 
+import java.io.IOException;
+
+import org.qifu.base.Constants;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class BlankController {
 	
 	@RequestMapping("/")
-	public String indexPage(ModelMap mm, HttpServletRequest request) {
+	public String indexPage(ModelMap mm, HttpServletRequest request, HttpServletResponse response) {
 		return "view/blank";
 	}
+	
+	@RequestMapping(value = "/" , method = RequestMethod.GET, produces = "application/json")
+	public String indexGet(ModelMap mm, HttpServletRequest request, HttpServletResponse response) {
+		try {
+			response.getWriter().print(Constants.NO_LOGIN_JSON_DATA);
+			response.getWriter().flush();
+			response.getWriter().close();			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		return null;
+	}	
 	
 	@RequestMapping("/loginPage")
 	public String loginPage(ModelMap mm, HttpServletRequest request) {
