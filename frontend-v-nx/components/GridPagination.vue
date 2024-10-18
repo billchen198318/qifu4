ff5dfd00bc1193e29d727c81051eb9e39eec71b9
billchen198318 <chen.xin.nien@gmail.com>
2024-10-18 22:43:06 +0800

    update

--- frontend-v-nx/components/GridPagination.vue
+++ frontend-v-nx/components/GridPagination.vue
@@ -117,55 +117,59 @@ export default {
 <div class="row">
     <div class="col-xs-12 col-md-12 col-lg-12">
         <table v-if=" this.gridConfig != null && this.gridConfig.total > 0 " width="100%" border="0" cellspacing="0" cellpadding="1" >
-            <tr>
-                <td  width="70%" align="left" valign="middle">
-                    <nav aria-label="Page navigation">
-                        <ul class="pagination">
-                            <li class="page-item" @click="this.changePageSelectMethod(1)">
-                                <a class="page-link" href="#">
-                                    <span aria-hidden="true">&laquo;</span>
-                                </a>
-                            </li>
-                            <li class="page-item" @click="this.changePageSelectMethod(this.prevPage)">
-                                <a class="page-link" href="#">
-                                    <span aria-hidden="true">&lsaquo;</span>
-                                </a>
-                            </li>
-                            <li v-for="pNum in pItem" v-bind:class="'page-item' + (pNum == this.gridConfig.page ? ' active ' : ' ')" @click="this.changePageSelectMethod(pNum)"><a class="page-link" href="#">{{pNum}}</a></li>
-                            <li class="page-item" @click="this.changePageSelectMethod(this.nexPage)">
-                                <a class="page-link" href="#">
-                                    <span aria-hidden="true">&rsaquo;</span>
-                                </a>
-                            </li>
-                            <li class="page-item" @click="this.changePageSelectMethod(this.lastPage)">
-                                <a class="page-link" href="#">
-                                    <span aria-hidden="true">&raquo;</span>
-                                </a>
-                            </li>
-                        </ul> 
-                    </nav>       	      
-	            </td>   
-                <td width="30%" align="right" valign="middle">
-                    <table width="100%" border="0" cellspacing="0" cellpadding="1" >
-                        <tr>
-                            <td align="right">
-                                <div class="btn-group me-2" role="group" aria-label="Second group">
-                                    <button type="button" v-bind:class="'btn btn-secondary ' + this.gBtnChangeRow1st" @click="changeGridConfigRowMethod(10)">10</button>
-                                    <button type="button" v-bind:class="'btn btn-secondary ' + this.gBtnChangeRow2st" @click="changeGridConfigRowMethod(30)">30</button>
-                                    <button type="button" v-bind:class="'btn btn-secondary ' + this.gBtnChangeRow3st" @click="changeGridConfigRowMethod(50)">50</button>
-                                    <button type="button" v-bind:class="'btn btn-secondary ' + this.gBtnChangeRow4st" @click="changeGridConfigRowMethod(100)">100</button>
-                                </div>
-                            </td>
-                            <td align="left">
-                                <h6>
-                                    Total
-                                    <span class="badge text-bg-info">{{this.gridConfig.total}}</span>
-                                </h6>
-                            </td>
-                        </tr>
-                    </table>
-                </td>             
-            </tr>
+            <tbody>
+                <tr>
+                    <td  width="70%" align="left" valign="middle">
+                        <nav aria-label="Page navigation">
+                            <ul class="pagination">
+                                <li class="page-item" @click="this.changePageSelectMethod(1)">
+                                    <a class="page-link" href="#">
+                                        <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                </li>
+                                <li class="page-item" @click="this.changePageSelectMethod(this.prevPage)">
+                                    <a class="page-link" href="#">
+                                        <span aria-hidden="true">&lsaquo;</span>
+                                    </a>
+                                </li>
+                                <li v-for="pNum in pItem" v-bind:class="'page-item' + (pNum == this.gridConfig.page ? ' active ' : ' ')" @click="this.changePageSelectMethod(pNum)"><a class="page-link" href="#">{{pNum}}</a></li>
+                                <li class="page-item" @click="this.changePageSelectMethod(this.nexPage)">
+                                    <a class="page-link" href="#">
+                                        <span aria-hidden="true">&rsaquo;</span>
+                                    </a>
+                                </li>
+                                <li class="page-item" @click="this.changePageSelectMethod(this.lastPage)">
+                                    <a class="page-link" href="#">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                </li>
+                            </ul> 
+                        </nav>       	      
+                    </td>   
+                    <td width="30%" align="right" valign="middle">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="1" >
+                            <tbody>
+                                <tr>
+                                    <td align="right">
+                                        <div class="btn-group me-2" role="group" aria-label="Second group">
+                                            <button type="button" v-bind:class="'btn btn-secondary ' + this.gBtnChangeRow1st" @click="changeGridConfigRowMethod(10)">10</button>
+                                            <button type="button" v-bind:class="'btn btn-secondary ' + this.gBtnChangeRow2st" @click="changeGridConfigRowMethod(30)">30</button>
+                                            <button type="button" v-bind:class="'btn btn-secondary ' + this.gBtnChangeRow3st" @click="changeGridConfigRowMethod(50)">50</button>
+                                            <button type="button" v-bind:class="'btn btn-secondary ' + this.gBtnChangeRow4st" @click="changeGridConfigRowMethod(100)">100</button>
+                                        </div>
+                                    </td>
+                                    <td align="left">
+                                        <h6>
+                                            Total
+                                            <span class="badge text-bg-info">{{this.gridConfig.total}}</span>
+                                        </h6>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>             
+                </tr>
+            </tbody>
         </table>
     </div>
 </div>
