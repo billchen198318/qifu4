eee74a1a379dd92f3f42b13559d1e42a3d748bd9
foreshot <17112861+foreshot@user.noreply.gitee.com>
2026-05-27 11:18:41 +0800

    update

--- backend/base/src/main/java/org/qifu/base/Constants.java
+++ backend/base/src/main/java/org/qifu/base/Constants.java
@@ -24,7 +24,6 @@ package org.qifu.base;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.UUID;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
